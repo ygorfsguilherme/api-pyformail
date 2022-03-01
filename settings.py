0cae49f217eef3f16b188cd0ed67e422b8d99942
M1R4I-N1KK1 <ygorf3535@gmail.com>
2022-03-01 09:55:04 -0300

    fixado erro em variavel

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import environ
 env = environ.Env()
 
-FROM_EMAIL = env('FROM_EMAIL')
-TO_EMAIL = env('TO_MAIL')
-PASS_EMAIL = env('PASS_EMAIL')
+FROM_EMAIL = env("FROM_EMAIL")
+TO_EMAIL = env("TO_EMAIL")
+PASS_EMAIL = env("PASS_EMAIL")
